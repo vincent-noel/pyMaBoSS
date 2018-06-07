f5316935cca797520954da63b7912c2ce031d6ee
Aurelien Naldi <aurelien.naldi@gmail.com>
2018-06-07 17:02:27 +0200

    Release 0.6.2

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from setuptools import setup, find_packages
 
 setup(name='maboss',
-    version="0.6.1",
+    version="0.6.2",
     packages=find_packages(),
     author="Nicolas Levy",
     author_email="nicolaspierrelevy@gmail.com",
-    description="A python and jupyter api for the MaBoSS software")
+    description="A python and jupyter API for the MaBoSS software")
