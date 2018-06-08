07109e7bf7c135709a478000591372ad24b5ad34
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2018-06-08 14:09:55 +0200

    pip packaging

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # pyMaBoSS
 
 [![Documentation Status](https://readthedocs.org/projects/pymaboss/badge/?version=latest)](http://pymaboss.readthedocs.io/en/latest/?badge=latest)
+[![PyPI version](https://badge.fury.io/py/maboss.svg)](https://badge.fury.io/py/maboss)
 
 Python interface for the MaBoSS software (https://maboss.curie.fr)
--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 
-
 from setuptools import setup, find_packages
 
 setup(name='maboss',
     version="0.6.2",
-    packages=find_packages(),
+    packages=find_packages(exclude=["test"]),
     author="Nicolas Levy",
     author_email="nicolaspierrelevy@gmail.com",
-    description="A python and jupyter API for the MaBoSS software")
+    description="A python and jupyter API for the MaBoSS software",
+    install_requires = [
+        "colomoto_jupyter",
+        "pyparsing",
+        "ipywidgets",
+        "matplotlib",
+        "pandas",
+    ])
