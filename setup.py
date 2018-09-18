c2935ca9248eac9067fc2b0801da6311a9bd3030
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2018-09-18 15:44:45 +0200

    python -m maboss_setup

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup, find_packages
 setup(name='maboss',
     version="0.6.2",
     packages=find_packages(exclude=["test"]),
+    py_modules = ["maboss_setup"],
     author="Nicolas Levy",
     author_email="nicolaspierrelevy@gmail.com",
     description="A python and jupyter API for the MaBoSS software",
