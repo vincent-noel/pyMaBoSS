1e0b2b41cbcd5e77075cd88df4c2e8db229404ed
vincent <contact@vincent-noel.fr>
2018-10-24 16:57:17 +0200

    SBML export, with an option to export maboss functions with proprietary tags

--- maboss/ast.py
+++ maboss/ast.py
@@ -7,26 +7,34 @@ import json
 
 class ASTNode(object):
 
-    def __init__(self, tokens):
-        self.tokens = tokens
-        self.identify()
+    def __init__(self, tokens=None):
+
+        if tokens is not None:
+            self.tokens = tokens
+            self.identify()
 
     def identify(self):
 
         if 'tokens' in self.__dict__.keys():
-            while len(self.tokens) == 1 and not isinstance(self.tokens[0], str) and 'tokens' in self.tokens[0].__dict__.keys():
+            while len(self.tokens) == 1 and not (isinstance(self.tokens[0], str) or isinstance(self.tokens[0], float)) and 'tokens' in self.tokens[0].__dict__.keys():
                 self.tokens = self.tokens[0].tokens
 
-            if len(self.tokens) == 1 and isinstance(self.tokens[0], str):
-                self.__class__ = ASTVar
+            if len(self.tokens) == 1 and isinstance(self.tokens[0], float) or isinstance(self.tokens[0], int):
+                self.__class__ = ASTReal
+
+            elif len(self.tokens) == 1 and isinstance(self.tokens[0], str):
+                if self.tokens[0].lower() in ['true', 'false']:
+                    self.__class__ = ASTBool
+                else:
+                    self.__class__ = ASTVar
 
             elif self.tokens[0] in ['~', '!', 'not', 'NOT']:
                 self.__class__ = ASTNot
 
-            elif len(self.tokens) > 1 and self.tokens[1] in ['&&', '&']:
+            elif len(self.tokens) > 1 and self.tokens[1] in ['&&', '&', 'AND']:
                 self.__class__ = ASTAnd
 
-            elif len(self.tokens) > 1 and self.tokens[1] in ['||', '|']:
+            elif len(self.tokens) > 1 and self.tokens[1] in ['||', '|', 'OR']:
                 self.__class__ = ASTOr
 
             elif len(self.tokens) > 1 and self.tokens[1] in ['^', 'XOR']:
@@ -37,21 +45,56 @@ class ASTNode(object):
 
             elif len(self.tokens) > 1 and self.tokens[0] == "(" and self.tokens[2] == ")":
 
-                self.__dict__ = json.loads(self.tokens[1].replace('\'', '"'))
-                if self.operator == 'not':
-                    self.__class__ = ASTNot
-                elif self.operator == 'and':
-                    self.__class__ = ASTAnd
-                elif self.operator == 'or':
-                    self.__class__ = ASTOr
-                elif self.operator == 'xor':
-                    self.__class__ = ASTXor
-                elif self.operator == 'ife':
-                    self.__class__ = ASTIFE
+                if isinstance(self.tokens[1], str):
+                    t_node = ASTNode()
+                    t_node.loadFromString(self.tokens[1].replace('\'', '"'))
+                    self.__class__ = self.tokens.asDict()['AST'].__class__
+                    self.__dict__ = self.tokens.asDict()['AST'].__dict__
+
+                else:
+                    self.__class__ = self.tokens[1].__class__
+                    self.__dict__ = self.tokens[1].__dict__
 
                 return
 
             self.assignFields()
+
+    def loadFromDict(self, read_dict):
+
+        self.operator = read_dict['operator']
+        self.args = []
+        for arg in read_dict['args']:
+            if not (isinstance(arg, str) or isinstance(arg, float) or isinstance(arg, int)):
+                new_node = ASTNode()
+                new_node.loadFromDict(arg)
+                self.args.append(new_node)
+            else:
+                self.args.append(arg)
+
+        if self.operator == 'not':
+            self.__class__ = ASTNot
+        elif self.operator == 'and':
+            self.__class__ = ASTAnd
+        elif self.operator == 'or':
+            self.__class__ = ASTOr
+        elif self.operator == 'xor':
+            self.__class__ = ASTXor
+        elif self.operator == 'ife':
+            self.__class__ = ASTIFE
+        elif self.operator == 'var':
+            self.__class__ = ASTVar
+        elif self.operator == 'bool':
+            self.__class__ = ASTBool
+        elif self.operator == 'real':
+            self.__class__ = ASTReal
+        else:
+            print("Unkown ASTNode type when importing from dict : %s" % self.operator)
+
+
+    def loadFromString(self, read_string):
+
+        t_dict = json.loads(read_string)
+        self.loadFromDict(t_dict)
 
     def __str__(self):
         return str(self.__dict__)
@@ -62,12 +105,78 @@ class ASTNode(object):
         self.__class__ = self.tokens[0].__class__
         self.__dict__ = self.tokens[0].__dict__
 
+    def listVars(self):
+        vars = []
+        if self.operator == 'var':
+            vars.append(self.args[0])
+        elif not (self.operator == 'real' or self.operator == 'bool'):
+            for arg in self.args:
+                vars += arg.listVars()
+        return vars
+
+    def copy(self):
+        new_node = ASTNode()
+        new_node.__class__ = self.__class__
+        new_node.operator = self.operator
+        new_node.args = []
+        for arg in self.args:
+            if isinstance(arg, ASTNode):
+                new_node.args.append(arg.copy())
+            else:
+                new_node.args.append(arg)
+        return new_node
+
+    def subs(self, logic, internal_vars):
+
+        if self.operator == 'var':
+
+            if self.args[0] == '@logic':
+                t_ast = logic.copy()
+                self.__class__ = t_ast.__class__
+                self.__dict__ = t_ast.__dict__
+
+            elif self.args[0] in internal_vars.keys():
+                t_ast = internal_vars[self.args[0]].copy()
+                self.__class__ = t_ast.__class__
+                self.__dict__ = t_ast.__dict__
+
+        elif self.operator not in ['real', 'bool']:
+            for arg in self.args:
+                arg.subs(logic, internal_vars)
+
+    def develop(self, logic, internal_vars):
+        new_node = self.copy()
+        new_node.subs(logic, internal_vars)
+        return new_node
+
+
+
+class ASTReal(ASTNode):
+
+    def assignFields(self):
+        self.operator = 'real'
+        self.args = [self.tokens[0]]
+        del self.tokens
+
+    def __str__(self):
+        return str(self.args[0])
+
+class ASTBool(ASTNode):
+
+    def assignFields(self):
+        self.operator = 'bool'
+        self.args = [self.tokens[0].upper()]
+        del self.tokens
+
+    def __str__(self):
+        return str(self.args[0])
+
 
 class ASTVar(ASTNode):
 
     def assignFields(self):
         self.operator = 'var'
-        self.args = self.tokens[0]
+        self.args = [self.tokens[0]]
         del self.tokens
 
     def __str__(self):
@@ -117,6 +226,7 @@ class ASTIFE(ASTNode):
 logExp = pp.Forward()
 
 boolCst = pp.oneOf("True False")
+realCst = pp.pyparsing_common.real.copy()
 boolNot = pp.oneOf("! ~ NOT")
 boolAnd = pp.oneOf("&& & AND")
 boolOr = pp.oneOf("|| | OR")
@@ -127,10 +237,10 @@ lparen = '('
 rparen = ')'
 
 varName = (
-        ~boolAnd + ~boolOr + ~boolXor + ~boolNot + ~boolCst + ~boolTest
-        + ~boolElse + ~pp.Literal('Node') + pp.Word(pp.alphas+'$', pp.alphanums+'_')
+        ~boolAnd + ~boolOr + ~boolXor + ~boolNot + ~boolCst + ~realCst + ~boolTest
+        + ~boolElse + ~pp.Literal('Node') + pp.Word(pp.alphas+'$'+'@', pp.alphanums+'_')
 ).setParseAction(ASTNode)
-logTerm = (boolCst | varName | (lparen + logExp + rparen).setParseAction(ASTNode)).setParseAction(ASTNode)
+logTerm = (boolCst | realCst | varName | (lparen + logExp + rparen)).setParseAction(ASTNode)
 logNot = (pp.ZeroOrMore(boolNot) + logTerm).setParseAction(ASTNode)
 logAnd = (logNot + pp.ZeroOrMore(boolAnd + logNot)).setParseAction(ASTNode)
 logOr = (logAnd + pp.ZeroOrMore(boolOr + logAnd)).setParseAction(ASTNode)
--- maboss/sbml.py
+++ maboss/sbml.py
@@ -1,41 +1,99 @@
 import libsbml
-import boolean
 import re
+from .ast import logExp as logExpAST
+from .ast import ASTReal, ASTVar, ASTBool, ASTNot, ASTAnd, ASTOr, ASTXor, ASTIFE
 
-algebra = boolean.BooleanAlgebra()
+uri_qual = 'http://www.sbml.org/sbml/level3/version1/qual/version1'
+uri_maboss = 'http://www.sbml.org/sbml/level3/version1/maboss/version1'
 
 
-def booleanToSBML(node):
+def booleanToSBML(node, forceBoolean=False):
 
-	if isinstance(node, boolean.boolean.Symbol):
+	if isinstance(node, ASTReal):
+		sbml_node = libsbml.ASTNode()
+		if forceBoolean:
+			if node.args == 1.0:
+				sbml_node.setType(libsbml.AST_CONSTANT_TRUE)
+			else:
+				sbml_node.setType(libsbml.AST_CONSTANT_FALSE)
+		else:
+			sbml_node.setType(libsbml.AST_REAL)
+			sbml_node.setValue(float(node.args[0]))
+
+		return sbml_node
+
+	elif isinstance(node, ASTBool):
+		sbml_node = libsbml.ASTNode()
+
+		if node.args[0] == 'TRUE':
+			sbml_node.setType(libsbml.AST_CONSTANT_TRUE)
+		elif node.args[0] == 'FALSE':
+			sbml_node.setType(libsbml.AST_CONSTANT_FALSE)
+		else:
+			print("Unrecognized boolean value : %s" % node.args[0])
+
+		return sbml_node
+
+	elif isinstance(node, ASTVar):
 		sbml_node = libsbml.ASTNode()
 		sbml_node.setType(libsbml.AST_NAME)
-		sbml_node.setName(str(node))
+		sbml_node.setName(node.args[0].replace('$', '_'))
 
-		return sbml_node
+		if forceBoolean:
+			sbml_one = libsbml.ASTNode()
+			sbml_one.setType(libsbml.AST_REAL)
+			sbml_one.setValue(1)
 
-	elif isinstance(node, boolean.boolean.NOT):
+			sbml_eq = libsbml.ASTNode()
+			sbml_eq.setType(libsbml.AST_RELATIONAL_EQ)
+			sbml_eq.addChild(sbml_node)
+			sbml_eq.addChild(sbml_one)
+
+			return sbml_eq
+		else:
+			return sbml_node
+
+	elif isinstance(node, ASTNot):
 		sbml_node = libsbml.ASTNode()
 		sbml_node.setType(libsbml.AST_LOGICAL_NOT)
-		sbml_node.addChild(booleanToSBML(node.args[0]))
-
+		sbml_node.addChild(booleanToSBML(node.args[0], forceBoolean))
 		return sbml_node
 
-	elif isinstance(node, boolean.boolean.AND):
+	elif isinstance(node, ASTAnd):
 		sbml_node = libsbml.ASTNode()
 		sbml_node.setType(libsbml.AST_LOGICAL_AND)
 
 		for arg in node.args:
-			sbml_node.addChild(booleanToSBML(arg))
+			sbml_node.addChild(booleanToSBML(arg, forceBoolean))
 
 		return sbml_node
 
-	elif isinstance(node, boolean.boolean.OR):
+	elif isinstance(node, ASTOr):
 		sbml_node = libsbml.ASTNode()
 		sbml_node.setType(libsbml.AST_LOGICAL_OR)
 
 		for arg in node.args:
-			sbml_node.addChild(booleanToSBML(arg))
+			sbml_node.addChild(booleanToSBML(arg, forceBoolean))
+
+		return sbml_node
+
+	elif isinstance(node, ASTXor):
+		sbml_node = libsbml.ASTNode()
+		sbml_node.setType(libsbml.AST_LOGICAL_XOR)
+
+		for arg in node.args:
+			sbml_node.addChild(booleanToSBML(arg, forceBoolean))
+
+		return sbml_node
+
+	elif isinstance(node, ASTIFE):
+
+		sbml_node = libsbml.ASTNode()
+		sbml_node.setType(libsbml.AST_FUNCTION_PIECEWISE)
+
+		sbml_node.addChild(booleanToSBML(node.args[1], forceBoolean))
+		sbml_node.addChild(booleanToSBML(node.args[0], forceBoolean))
+		sbml_node.addChild(booleanToSBML(node.args[2], forceBoolean))
 
 		return sbml_node
 
@@ -47,165 +105,142 @@ def addQualitativeSpecies(sbml_model, sid, species, initial_state):
 	new_species.setName(species.name)
 	new_species.setId(sid)
 	new_species.setInitialLevel(initial_state)
+	new_species.setCompartment("cell")
+	new_species.setConstant(False)
 	# sbml_model.getPlugin("qual").addQualitativeSpecies(new_species)
 
-def addContinuousSpecies(sbml_model, sid, species, initial_state):
-	new_species = sbml_model.createSpecies()
-	new_species.setName(species.name + " (continuous)")
-	new_species.setId(sid + "_cont")
-	new_species.setInitialAmount(initial_state)
-	new_species.setCompartment("cell")
-	sbml_model.addSpecies(new_species)
-
-
-def parseFormula(formula, species):
-	if '?' in formula and ":" in formula:
-		res_match = re.match("(.+)\?(.*):(.*)", formula)
-		(formula, true, false) = tuple([element.strip() for element in res_match.groups()])
-		if '@logic' in formula:
-			formula = formula.replace('@logic', species.logExp)
-		# print(formula)
-		return (formula, true, false)
-	elif isValue(species.rt_up):
-		rateUp = float(species.rt_up)
-		return (True, rateUp, None)
-	else:
-		raise Exception("formula not recognized : %s" % str(formula))
-
-def isValue(value):
-	try:
-		float(value)
-		return True
-	except:
-		return False
-
+def getAST(formula):
+	return logExpAST.parseString(formula).asDict()['AST']
 
 def getInputs(formula, species):
 
-	if '?' in formula and ":" in formula:
-		res_match = re.match("(.+)\?(.*):(.*)", formula)
-		(formula, true, false) = tuple([element.strip() for element in res_match.groups()])
-		if '@logic' in formula:
-			formula = formula.replace('@logic', species.logExp)
-		#print(formula)
-		return algebra.parse(formula).symbols
-	elif isValue(species.rt_up):
-		rateUp = float(species.rt_up)
-		return {}
-	else:
-		raise Exception("formula not recognized : %s" % str(formula))
+	i_var = {("@%s" % key): getAST(value) for key, value in species.internal_var.items()}
+	developped_res = getAST(formula).develop(getAST(species.logExp), i_var)
+	list_vars = [var for var in developped_res.listVars() if not var.startswith('$')]
+	return set(list_vars)
 
-def addTransition(sid, species, model):
+def addTransition(model, sid, species):
 	transition = model.getPlugin("qual").createTransition()
+	transition.setId("transitions_%s" % species.name)
 
 	output = transition.createOutput()
 	output.setQualitativeSpecies(sid)
 	output.setTransitionEffect(libsbml.OUTPUT_TRANSITION_EFFECT_ASSIGNMENT_LEVEL)
 
-	inputs = getInputs(species.rt_up.replace("$", "_"), species)
-	inputs.union(getInputs(species.rt_down.replace("$", "_"), species))
+	inputs = getInputs(species.rt_up, species)
+	inputs.union(getInputs(species.rt_down, species))
 
 	for input in inputs:
 		sbml_input = transition.createInput()
 		sbml_input.setQualitativeSpecies(str(input))
 		sbml_input.setTransitionEffect(libsbml.INPUT_TRANSITION_EFFECT_NONE)
 
-	formula, _, _ = parseFormula(species.rt_up, species)
+	internal_var = {("@%s" % key): getAST(value) for key, value in species.internal_var.items()}
 
 	terms_up = transition.createFunctionTerm()
 	terms_up.setResultLevel(1)
-	terms_up.setMath(booleanToSBML(algebra.parse(formula.replace('$', '_'))))
 
-	formula, _, _ = parseFormula(species.rt_down, species)
+	dev_formula_up = getAST(species.rt_up).develop(getAST(species.logExp), internal_var)
+	sbml_formula = booleanToSBML(dev_formula_up)
+	terms_up.setMath(sbml_formula)
 
 	terms_down = transition.createFunctionTerm()
 	terms_down.setResultLevel(0)
-	terms_down.setMath(booleanToSBML(algebra.parse(formula.replace('$', '_'))))
 
+	dev_formula_down = getAST(species.rt_down).develop(getAST(species.logExp), internal_var)
+	sbml_formula = booleanToSBML(dev_formula_down)
+	terms_down.setMath(sbml_formula)
 
+	default = transition.createDefaultTerm()
+	default.setResultLevel(0)
 
-def addRateValue(value):
+def findTransition(xml_model, id):
 
-	try:
+	transitions = xml_model.getChild('listOfTransitions')
+	i = 0
+	while i < transitions.getNumChildren():
+		t_child = transitions.getChild(i)
 
-		float_value = libsbml.ASTNode()
-		float_value.setType(libsbml.AST_REAL)
-		float_value.setValue(float(value))
-		return float_value
+		if t_child.hasAttr('id', uri_qual) and t_child.getAttrValue('id', uri_qual) == id:
+			break
+		i += 1
 
-	except ValueError as e:
-		var_value = libsbml.ASTNode()
-		var_value.setType(libsbml.AST_NAME)
-		var_value.setName(value.replace("$", "_"))
-		return var_value
-
-def buildSBMLRate(rate, species):
-
-	formula, true, false = parseFormula(rate, species)
-	boolean_formula = algebra.parse(formula.replace("$", "_"))
-	sbml_formula = libsbml.ASTNode()
-	if isValue(formula):
-		sbml_formula.setType(libsbml.AST_REAL)
-		sbml_formula.setValue(float(formula))
-
+	if i < transitions.getNumChildren():
+		return transitions.getChild(i)
 	else:
-		sbml_formula.setType(libsbml.AST_FUNCTION_PIECEWISE)
-		sbml_formula.addChild(addRateValue(true))
-		sbml_formula.addChild(booleanToSBML(boolean_formula))
-		sbml_formula.addChild(addRateValue(false))
+		print("Unable to find transition with id == %s" % ('transitions_%s' % id))
 
-	return sbml_formula
+def findFunction(xml_transition, resultLevel):
 
-def getModifiers(formula, species):
-	modifiers = []
+	functions = xml_transition.getChild('listOfFunctionTerms')
+	j = 0
+	while j < functions.getNumChildren():
+		t_function = functions.getChild(j)
+		if t_function.hasAttr('resultLevel', uri_qual) and t_function.getAttrValue('resultLevel', uri_qual) == resultLevel:
+			break
+		j += 1
 
-	t_formula, _, _ = parseFormula(formula, species)
-	boolean_formula = algebra.parse(t_formula.replace("$", "_"))
-	modifiers = [str(var) for var in boolean_formula.symbols if not str(var).startswith("_")]
-	#
-	# formula, _, _ = parseFormula(species.rt_down, species)
-	# boolean_formula = algebra.parse(formula.replace("$", "_"))
-	# modifiers += [str(var) for var in boolean_formula.symbols if not str(var).startswith("_")]
+	if j < functions.getNumChildren():
 
-	return list(set(modifiers))
+		return functions.getChild(j)
+	else:
+		print("unable to find function with resultLevel == %s" % resultLevel)
 
-def addReaction(sbml_model, sid, species):
+def addMaBoSSMathToTransitions(xml_model, sid, species):
 
-	new_reaction = sbml_model.createReaction()
-	# Here reactions are simple : reversible synthesis/degradation
-	new_reaction.setId("production_%s" % sid)
-	new_reaction.setReversible(False)
-	product = new_reaction.createProduct()
-	product.setSpecies(sid)
+	transition = findTransition(xml_model, "transitions_%s" % species.name)
 
-	modifiers = getModifiers(species.rt_up, species)
-	for modifier in modifiers:
-		new_modifier = new_reaction.createModifier()
-		new_modifier.setSpecies(modifier)
+	function_activation = findFunction(transition, "1")
+	maboss_mathml = function_activation.getChild(0).clone()
+	maboss_mathml.setTriple(libsbml.XMLTriple("math", uri_maboss, "maboss"))
+	function_activation.addChild(maboss_mathml)
 
-	up_formula = buildSBMLRate(species.rt_up, species)
+	function_inactivation = findFunction(transition, "0")
+	maboss_mathml = function_inactivation.getChild(0).clone()
+	maboss_mathml.setTriple(libsbml.XMLTriple("math", uri_maboss, "maboss"))
+	function_inactivation.addChild(maboss_mathml)
 
-	kinetic_law = new_reaction.createKineticLaw()
-	kinetic_law.setMath(up_formula)
-	# new_reaction.setKineticLaw(sbml_formula)
-	sbml_model.addReaction(new_reaction)
+def findSBMLTransitionFunction(transition, resultLevel):
 
-	new_reaction = sbml_model.createReaction()
-	new_reaction.setId("degradation_%s" % sid)
-	new_reaction.setReversible(False)
-	reactant = new_reaction.createReactant()
-	reactant.setSpecies(sid)
+	i = 0
+	while i < transition.getNumFunctionTerms():
+		if transition.getFunctionTerm(i).getResultLevel() == resultLevel:
+			break
+		i += 1
 
-	modifiers = getModifiers(species.rt_down, species)
-	for modifier in modifiers:
-		new_modifier = new_reaction.createModifier()
-		new_modifier.setSpecies(modifier)
+	if i < transition.getNumFunctionTerms():
+		return transition.getFunctionTerm(i)
+	else:
+		print("Unable to find the function term with resultLevel == %s" % resultLevel)
 
-	down_formula = buildSBMLRate(species.rt_down, species)
-	kinetic_law = new_reaction.createKineticLaw()
-	kinetic_law.setMath(down_formula)
-	sbml_model.addReaction(new_reaction)
+def forceBooleanMath(formula):
 
+	if formula.operator == 'ife':
+		return formula.args[0]
+
+	elif formula.operator == 'bool':
+		return formula.args[0]
+
+	elif formula.operator == 'real':
+		if formula.args[0] == 0:
+			return ASTBool(['false'])
+		else:
+			return ASTBool(['true'])
+
+	return formula
+
+def switchToBooleanTransitions(sbml_model, sid, species):
+
+	internal_var = {("@%s" % key): getAST(value) for key, value in species.internal_var.items()}
+	transition = sbml_model.getPlugin("qual").getElementBySId("transitions_%s" % species.name)
+
+	activation = findSBMLTransitionFunction(transition, 1)
+	dev_formula_up = getAST(species.rt_up).develop(getAST(species.logExp), internal_var)
+	activation.setMath(booleanToSBML(forceBooleanMath(dev_formula_up), forceBoolean=True))
+
+	inactivation = findSBMLTransitionFunction(transition, 0)
+	dev_formula_down = getAST(species.rt_down).develop(getAST(species.logExp), internal_var)
+	inactivation.setMath(booleanToSBML(forceBooleanMath(dev_formula_down), forceBoolean=True))
 
 def addParameter(sbml_model, name, value):
 
@@ -213,9 +248,10 @@ def addParameter(sbml_model, name, value):
 	new_param.setName(name[1: len(name)])
 	new_param.setId("_%s" % name[1: len(name)])
 	new_param.setValue(value)
+	new_param.setConstant(True)
 	sbml_model.addParameter(new_param)
 
-def to_sbml(model, filename):
+def to_sbml(model, filename, maboss_extras=True):
 
 	sbml_doc = libsbml.SBMLDocument(3, 1)
 	sbml_doc.enablePackage("http://www.sbml.org/sbml/level3/version1/qual/version1", "qual", True)
@@ -225,19 +261,34 @@ def to_sbml(model, filename):
 	compartment = sbml_model.createCompartment()
 	compartment.setName("Cell")
 	compartment.setId("cell")
+	compartment.setConstant(True)
 
 	initial_state = model.get_initial_state()
 
 	for sid, species in model.network.items():
 		addQualitativeSpecies(sbml_model, sid, species, initial_state[sid])
-		addTransition(sid, species, sbml_model)
-
-		# addContinuousSpecies(sbml_model, sid, species, initial_state[sid])
-		# addReaction(sbml_model, sid, species)
+		addTransition(sbml_model, sid, species)
 
 	for param, value in model.param.items():
 		if param.startswith("$"):
 			addParameter(sbml_model, param, float(value))
+
+	if maboss_extras:
+		xml_doc = sbml_doc.toXMLNode()
+		xml_doc.addAttr(libsbml.XMLTriple("maboss", uri_maboss, "xmlns"), uri_maboss)
+		xml_doc.addAttr(libsbml.XMLTriple("required", uri_maboss, "maboss"), "true")
+
+		xml_model = xml_doc.getChild('model')
+
+		for sid, species in model.network.items():
+			addMaBoSSMathToTransitions(xml_model, sid, species)
+
+
+		sbml_doc = libsbml.readSBMLFromString(xml_doc.toXMLString())
+		sbml_model = sbml_doc.getModel()
+
+	for sid, species in model.network.items():
+		switchToBooleanTransitions(sbml_model, sid, species)
 
 	libsbml.writeSBMLToFile(sbml_doc, filename)
 	return sbml_doc
