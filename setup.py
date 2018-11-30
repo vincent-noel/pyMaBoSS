07f51830b6f060c8806a9638ebc7045f3fd00954
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2018-11-30 12:15:32 +0100

    release 0.6.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='maboss',
-    version="0.6.3",
+    version="0.6.4",
     packages=find_packages(exclude=["test"]),
     py_modules = ["maboss_setup"],
     author="Nicolas Levy",
