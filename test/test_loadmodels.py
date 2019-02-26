17602d2f640c51b90942f8332220891daf8aa0e9
Vincent Noël <contact@vincent-noel.fr>
2019-02-26 18:10:42 +0100

    Adding basic tests with Travis+Coveralls

--- test/test_loadmodels.py
+++ test/test_loadmodels.py
@@ -1,8 +1,13 @@
 """Test suite for loading models."""
 
-
+from unittest import TestCase
 from maboss import load
 from os.path import dirname, join
 
-sim = load(join(dirname(__file__), "p53_Mdm2.bnd"), join(dirname(__file__), "p53_Mdm2_runcfg.cfg"))
-assert(sim.network['Mdm2C'].logExp == "$case_a ? p53_h : p53")
+
+class TestLoadModels(TestCase):
+
+	def test_load_p53_Mdm2(self):
+
+		sim = load(join(dirname(__file__), "p53_Mdm2.bnd"), join(dirname(__file__), "p53_Mdm2_runcfg.cfg"))
+		self.assertEqual(sim.network['Mdm2C'].logExp, "$case_a ? p53_h : p53")
