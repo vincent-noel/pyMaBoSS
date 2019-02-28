727d8ff556bbf711922a9a51c714b25917382913
Vincent Noël <contact@vincent-noel.fr>
2019-02-28 12:12:10 +0100

    Added a test to check the model

--- test/test_loadmodels.py
+++ test/test_loadmodels.py
@@ -31,3 +31,9 @@ class TestLoadModels(TestCase):
 
 		self.assertEqual(str(sim.network), str(sim_copy.network))
 		self.assertEqual(sim.str_cfg(), sim_copy.str_cfg())
+
+	def test_check_p53_Mdm2(self):
+		sim = load(join(dirname(__file__), "p53_Mdm2.bnd"), join(dirname(__file__), "p53_Mdm2_runcfg.cfg"))
+		errors = sim.check()
+		self.assertEqual(len(errors), 0)
+
