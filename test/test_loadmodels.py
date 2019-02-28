e50c412daa24c9499297175dfeb139518cb39751
Vincent Noël <contact@vincent-noel.fr>
2019-02-28 11:48:14 +0100

    Added a test for copy()

--- test/test_loadmodels.py
+++ test/test_loadmodels.py
@@ -11,3 +11,19 @@ class TestLoadModels(TestCase):
 
 		sim = load(join(dirname(__file__), "p53_Mdm2.bnd"), join(dirname(__file__), "p53_Mdm2_runcfg.cfg"))
 		self.assertEqual(sim.network['Mdm2C'].logExp, "$case_a ? p53_h : p53")
+
+	def test_simulate_p53_Mdm2(self):
+
+		sim = load(join(dirname(__file__), "p53_Mdm2.bnd"), join(dirname(__file__), "p53_Mdm2_runcfg.cfg"))
+		res = sim.run()
+
+		res.plot_fixpoint()
+		res.plot_node_trajectory()
+		res.plot_piechart()
+
+	def test_copy_p53_Mdm2(self):
+		sim = load(join(dirname(__file__), "p53_Mdm2.bnd"), join(dirname(__file__), "p53_Mdm2_runcfg.cfg"))
+		sim_copy = sim.copy()
+
+		self.assertEqual(str(sim.network), str(sim_copy.network))
+		self.assertEqual(sim.str_cfg(), sim_copy.str_cfg())
