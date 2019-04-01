d7c7a81216e5ab30d72ac978f119de2a5da873c4
Vincent Noël <contact@vincent-noel.fr>
2019-04-01 12:47:07 +0200

    Now using default parameters when no config file is given

--- maboss/ensemble/result.py
+++ maboss/ensemble/result.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 from ..result import BaseResult
+from ..simulation import _default_parameter_list
 import os
 import tempfile
 import subprocess
@@ -8,13 +9,17 @@ import sys
 
 class EnsembleResult(BaseResult):
 
-    def __init__(self, path, cfg_filename):
+    def __init__(self, path, cfg_filename=None):
         BaseResult.__init__(self)
 
         self.models_path = path
-        self._cfg = cfg_filename
-
+        self.param = _default_parameter_list
         self._path = tempfile.mkdtemp()
+        if cfg_filename is None:
+            self._cfg = os.path.join(self._path, "model.cfg")
+            self.write_cfg()
+        else:
+            self._cfg = cfg_filename
         self.models_files = [os.path.join(self.models_path, file) for file in os.listdir(self.models_path)]
         
         maboss_cmd = "MaBoSS"
@@ -44,3 +49,10 @@ class EnsembleResult(BaseResult):
     def get_statdist_file(self):
         return os.path.join(self._path, "res_statdist.csv")
         
+    def write_cfg(self):
+        
+        with open(os.path.join(self._path, "model.cfg", 'w')) as cfg_file:
+            for param, value in self._param.items():
+                cfg_file.write("%s = %s;" % (
+                    param, value
+                ))
