3645dcd98d20bd9fc2c0d22f684bd1827a89a8aa
Vincent Noël <contact@vincent-noel.fr>
2019-06-28 15:13:18 +0200

    Added test for overwrite in upp

--- test/test_uppmaboss.py
+++ test/test_uppmaboss.py
@@ -33,8 +33,23 @@ class TestUpPMaBoSS(TestCase):
 		
 		for i, pop_ratio in enumerate(pop_ratios):
 			self.assertAlmostEqual(pop_ratio, expected_pop_ratios[i], delta=pop_ratio/1e-6)
-		
-		resume = uppmaboss_sim.save('results')
+
+		# Now again, but with a modified max_time, and with overwrite
+		# Results should be different if overwriting indeed works
+		sim.param["max_time"] = 2
+		expected_pop_ratios_2 = [
+			1.0, 0.6284629999999846, 0.5871421862129369, 0.5177736854805769, 0.4238583410870101, 0.34499186727759734,
+			0.29076190567646526, 0.2539398179415862, 0.2271176786113088, 0.20601231409098322, 0.18811025602109335,
+			0.1740761022603733, 0.16198947625212115
+		]
+
+		uppmaboss_model = UpdatePopulation(sim, join(dirname(__file__), "CellFate_1h.upp"))
+		uppmaboss_sim = uppmaboss_model.run('WT', overwrite=True)
+		pop_ratios = uppmaboss_sim.get_population_ratios('WT').values.tolist()
+
+		for i, pop_ratio in enumerate(pop_ratios):
+			self.assertAlmostEqual(pop_ratio, expected_pop_ratios_2[i], delta=pop_ratio / 1e-6)
+
+		uppmaboss_sim.save('results')
 
 		uppmaboss_sim.results[0].plot_piechart()
-		
