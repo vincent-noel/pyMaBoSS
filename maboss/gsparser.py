6887b0485a3b4ff02c3766ec194ef7bb0ab74140
Vincent Noël <contact@vincent-noel.fr>
2019-07-17 11:13:36 +0200

    Removed variable list from network constructor

--- maboss/gsparser.py
+++ maboss/gsparser.py
@@ -123,11 +123,7 @@ def load(bnd_filename, *cfg_filenames):
 
         nodes = _read_bnd(bnd_content, is_internal_list)
 
-        # Some boolean variables can be defined in the .cfg and used in the .bnd
-        # We need to know about them when checking the logical formulae.
-        boolean_variables = ["$%s" % var for var, value in variables.items() if value in ['TRUE', 'FALSE']]
-
-        net = Network(nodes, boolean_variables)
+        net = Network(nodes)
         for istate in istate_list:
             net.set_istate(istate, istate_list[istate])
         for v in variables:
