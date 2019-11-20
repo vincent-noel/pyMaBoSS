3656045be6ac04b3ff807e233dea269573b8dac1
vnoel <vincent.noel@curie.fr>
2019-11-20 17:14:44 +0100

    Version bump to 0.7.9

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if version_info[0] < 3:
     optional_contextlib.append("contextlib2")
 
 setup(name='maboss',
-    version="0.7.8",
+    version="0.7.9",
     packages=find_packages(exclude=["test"]),
     py_modules = ["maboss_setup"],
     author="Nicolas Levy",
