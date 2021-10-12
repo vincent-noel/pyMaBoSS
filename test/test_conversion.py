f7dc54a524555b676f23b346ce5aa3bfe0e09c65
Vincent Noël <contact@vincent-noel.fr>
2021-10-12 19:09:43 +0200

    Removed ginsim-python install from tests

--- test/test_conversion.py
+++ test/test_conversion.py
@@ -11,16 +11,16 @@ from os.path import dirname, join
 
 class TestConvertModels(TestCase):
 
-	def test_convert_p53_Mdm2(self):
+	# def test_convert_p53_Mdm2(self):
 
-		model = load(join(dirname(__file__), "p53_Mdm2.bnd"), join(dirname(__file__), "p53_Mdm2_runcfg.cfg"))
-		with self.assertRaises(Exception) as context:
-			to_biolqm(model)
+	# 	model = load(join(dirname(__file__), "p53_Mdm2.bnd"), join(dirname(__file__), "p53_Mdm2_runcfg.cfg"))
+	# 	with self.assertRaises(Exception) as context:
+	# 		to_biolqm(model)
 
-	def test_convert_reprod_all(self):
+	# def test_convert_reprod_all(self):
 
-		model = load(join(dirname(__file__), "reprod_all.bnd"), join(dirname(__file__), "reprod_all.cfg"))
-		to_biolqm(model)
+	# 	model = load(join(dirname(__file__), "reprod_all.bnd"), join(dirname(__file__), "reprod_all.cfg"))
+	# 	to_biolqm(model)
 
 	def test_convert_reprod_all_minibn(self):
 
