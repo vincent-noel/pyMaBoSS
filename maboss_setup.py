8aa0a0fdad354b514906e55b30e04a1db0a59300
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2022-01-12 16:21:35 +0100

    pip install: install libsbml binary library

--- maboss_setup.py
+++ maboss_setup.py
@@ -1,3 +1,7 @@
+
+from ctypes.util import find_library
 from colomoto.setup_helper import setup
 setup({"pkg": "colomoto/maboss",
-    "check_progs": ["MaBoSS"]})
+    "check_progs": ["MaBoSS"]},
+    {"pkg": "colomoto/libsbml-plus-packages",
+        "check_install": lambda: find_library("sbml")})
