6157330638c54cba875906aaf038e03a95fb26ec
Vincent Noël <vincent.noel@curie.fr>
2023-05-26 16:59:04 +0200

    Bumped version to 0.8.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if version_info[0] < 3:
     optional_contextlib.append("contextlib2")
 
 setup(name='maboss',
-    version="0.8.4",
+    version="0.8.5",
     packages=find_packages(exclude=["test"]),
     py_modules = ["maboss_setup"],
     author="Nicolas Levy",
