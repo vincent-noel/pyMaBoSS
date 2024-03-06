5603d0f2cf7bf84db0484b67082b4391132c676c
Vincent Noël <vincent.noel@curie.fr>
2024-03-06 07:58:16 -0300

    Bumped version to 0.8.7

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "description.rst").read_text()
 
 setup(name='maboss',
-    version="0.8.6",
+    version="0.8.7",
     packages=find_packages(exclude=["test"]),
     py_modules = ["maboss_setup"],
     author="Vincent Noël, Loic Paulevé, Aurelien Naldi and Nicolas Levy",
