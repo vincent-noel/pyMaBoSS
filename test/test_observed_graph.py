c85fde12cb36508df86524f4748238608328f7ca
Vincent Noël <vincent.noel@curie.fr>
2024-09-11 17:45:13 -0300

    Added cmaboss tests for observed graph

--- test/test_observed_graph.py
+++ test/test_observed_graph.py
@@ -43,3 +43,33 @@ class TestObservedGraph(TestCase):
 			res.get_observed_graph().values, ref_observed_graph
 		).all())
 		
+	def test_cell_cycle_cmaboss(self):
+
+		path = dirname(__file__)
+		sim = load(join(path, "cellcycle.bnd"), join(path, "cellcycle_runcfg.cfg"))
+		sim.update_parameters(use_physrandgen = False)
+		sim.network.set_observed_graph_nodes(["CycD", "CycA", "CycE"])
+		res = sim.run(cmaboss=True)
+
+		ref_observed_graph = numpy.array([
+			[0.        , 0.        , 0.        , 0.        , 0.        ,
+				0.        , 0.        , 0.        ],
+			[0.        , 0.        , 0.        , 0.50555257, 0.        ,
+				0.49444743, 0.        , 0.        ],
+			[0.        , 0.        , 0.        , 0.        , 0.        ,
+				0.        , 0.        , 0.        ],
+			[0.        , 0.00537443, 0.        , 0.        , 0.        ,
+				0.        , 0.        , 0.99462557],
+			[0.        , 0.        , 0.        , 0.        , 0.        ,
+				0.        , 0.        , 0.        ],
+			[0.        , 0.94614513, 0.        , 0.        , 0.        ,
+				0.        , 0.        , 0.05385487],
+			[0.        , 0.        , 0.        , 0.        , 0.        ,
+				0.        , 0.        , 0.        ],
+			[0.        , 0.        , 0.        , 0.00715549, 0.        ,
+				0.99284451, 0.        , 0.        ]
+		])
+
+		self.assertTrue(numpy.isclose(
+			res.get_observed_graph().values, ref_observed_graph
+		).all())
