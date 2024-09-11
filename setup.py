cd58c45fedcddefdfa92659c66c1bd29e11ac8df
Vincent Noël <vincent.noel@curie.fr>
2024-09-11 17:50:12 -0300

    Bumped dependency for maboss and cmaboss, added networkx and pygraphviz dependency for conda"

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(name='maboss',
         "pandas",
         "scikit-learn",
         "networkx",
-        "cmaboss>=1.0.0b24"
+        "cmaboss>=1.0.0b25"
     ] + optional_contextlib,
     scripts=['scripts/MBSS_FormatTable.py', 'scripts/UpPMaBoSS.py']
 )
