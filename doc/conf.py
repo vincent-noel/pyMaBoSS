b8c9440d836fd4942c55a8cbb80842cb32d11b87
Vincent Noël <vincent.noel@curie.fr>
2025-08-28 11:27:21 -0300

    Added example notebook for node scheduling, accessible from the documentation

--- doc/conf.py
+++ doc/conf.py
@@ -37,7 +37,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'nbsphinx']
+    'nbsphinx', 'nbsphinx_link']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
