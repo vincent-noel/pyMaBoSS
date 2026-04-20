5b32e201b53a9bed88092b61ad808cadbb47b9ad
Oscar Dufossez <oscar.dufossez@edu.univ-fcomte.fr>
2026-04-20 10:09:40 +0200

    tested the logical expression parsing, it works

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,32 +1,8 @@
-from maboss import result
-from maboss.temporal_logic.evaluator import MaBoSSEvaluator
 from maboss.temporal_logic.logical_expression_compute import ComputeLogicalExpression
 from maboss.temporal_logic.temporal_parser import *
 from maboss.temporal_logic.formulas import *
 from maboss.temporal_logic.custom_exceptions import *
-from maboss.results.probtrajresult import ProbTrajResult
-from maboss.results.statdistresult import StatDistResult
 from unittest import TestCase
-from unittest.mock import patch
-import pandas as pd
-import numpy as np
-import os
-
-#Generation of a random dataframe
-class FakeResult:
-    def __init__(self, nodes_df, states_df, traj_df):
-        self._nodes_df = nodes_df
-        self._states_df = states_df
-        self._traj_df = traj_df
-
-    def get_nodes_probtraj(self):
-        return self._nodes_df
-
-    def get_states_probtraj(self):
-        return self._states_df
-
-    def get_probtraj(self):
-        return self._traj_df
 
 # Parser test queries
 QUERY = "P(node:name) <= 0.623"
@@ -68,11 +44,7 @@ LOGICAL_EXPRESSION_ERROR_SYMBOL = ['A', '|' , '&', 'B']
 LOGICAL_EXPRESSION_ERROR_FIRST_MEMBER_SYMBOL = ['&','A', '|', 'B']
 LOGICAL_EXPRESSION_ERROR_LAST_MEMBER_SYMBOL = ['A', '|', '&', 'B', '|']
 LOGICAL_EXPRESSION_ERROR_NODES_INTRICATE = ['A','|', ['B','&','C','D']]
-# DO NOT REMOVE ELSE BUGS -----------------
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-def get_test_path(filename):
-    return os.path.join(BASE_DIR, filename)
-# -------------------------------------------------
+
 # -------------------------- TESTS PARSER --------------------------------------
 
 class TestParser(TestCase):
@@ -184,39 +156,8 @@ class TestParser(TestCase):
         self.assertRaises(ErrorInLogicalExpression, ComputeLogicalExpression.check_logical_expression, LOGICAL_EXPRESSION_ERROR_LAST_MEMBER_SYMBOL)
         self.assertRaises(ErrorInLogicalExpression, ComputeLogicalExpression.check_logical_expression, LOGICAL_EXPRESSION_ERROR_NODES_INTRICATE)
 
-
-# -------------------- TEST WITH PROBAS AND NODES ------------------------------------------
-    def test_get_df_target_node(self):
-        df_nodes = pd.read_csv(get_test_path('test_data.csv'))
-        fake = FakeResult(df_nodes,None,None)
-        MaBoSSEvaluator.simulation_results = fake
-        df = MaBoSSEvaluator.get_df_target(TargetType.NODE)
-
-        assert df.equals(df_nodes)
-
-    def test_get_df_target_name(self):
-        df_nodes = pd.read_csv(get_test_path('test_data.csv'))
-        result = MaBoSSEvaluator.get_df_target_name(df_nodes, ["AKT2"])
-        print(result)
-        assert list(result.columns) == ["Time","AKT2"]
-
-    def test_get_df_target_value(self):
-        df = pd.read_csv(get_test_path('test_data.csv'))
-        MaBoSSEvaluator.operator_query = Operators.GT
-        MaBoSSEvaluator.target_name = "AKT3"
-        df=df[["Time","AKT3"]]
-        result = MaBoSSEvaluator.get_df_target_value_proba(df, 0.5)
-        expected = pd.read_csv(get_test_path('expected_data_target_value.csv'))
-        print(result)
-        print(expected)
-        assert result.reset_index(drop=True).equals(expected)
-
-    def test_get_df_full_process(self):
-        df_nodes = pd.read_csv(get_test_path('test_data.csv'))
-        result = MaBoSSEvaluator.querying(QUERY_FOUND_BY_NAME, FakeResult(df_nodes,None,None))
-        expected = pd.read_csv(get_test_path('expected_data_full_process.csv'))
-        print(result)
-        print(expected)
-        assert result.reset_index(drop=True).equals(expected)
+    def test_counting_members_logical_query(self):
+        assert Parser.counting_members_logical_query(LOGICAL_EXPRESSION_SIMPLE_NO_ERROR) == 3
+        assert Parser.counting_members_logical_query(LOGICAL_EXPRESSION_INTRICATE_NO_ERROR) == 5
 
 
